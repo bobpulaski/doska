d4b7eec404b717966b097dc35693e91b19ce60c6
Bob Pulaski <mynameisempty@yandex.ru>
2021-12-22 15:54:23 +0300

    22-12-21_job_15-54

--- resources/views/terrasnaja-doska-cm-decking.blade.php
+++ resources/views/terrasnaja-doska-cm-decking.blade.php
@@ -6,35 +6,92 @@
     <x-h1>Террасная доска из ДПК</x-h1>
     <x-h2>CM Decking</x-h2>
 
-    <div class="row row-cols-3 p-5">
+    <div class="container gx-5 p-4">
+        <div class="row">
+            <div class="col">
+                <div class="border p-5">
+                    <a href="#">
+                        <img class="img-fluid" src="{{ asset('images/cm-decking-vintage-solid.jpg') }}"
+                             alt="Террасная доска из ДПК CM Decking Vintage Solid">
+                    </a>
+                    <h3 class="display-6 text-center text-decoration-none">
+                        CM Decking
+                        <em class="text-muted">Vintage Solid</em>
+                    </h3>
+                </div>
+            </div>
 
-        <div class="col p-5">
-            <a href="#">
-                <img class="img-fluid" src="{{ asset('images/IMG_6172_1.png') }}" alt="">
-            </a>
-            <h3 class="display-6 text-center text-decoration-none">CM Decking Vintage Solid</h3>
+            <div class="col">
+                <div class="border p-5">
+                    <a href="#">
+                        <img class="img-fluid" src="{{ asset('images/cm-decking-zebrano.jpg') }}"
+                             alt="Террасная доска из ДПК CM Decking Zebrano">
+                    </a>
+                    <h3 class="display-6 text-center text-decoration-none">
+                        CM Decking
+                        <em class="text-muted">Zebrano</em>
+                    </h3>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="border p-5">
+                    <a href="#">
+                        <img class="img-fluid" src="{{ asset('images/cm-deking-natur-dub-double.jpg') }}"
+                             alt="Террасная доска из ДПК CM Decking Natur">
+                    </a>
+                    <h3 class="display-6 text-center text-decoration-none">
+                        CM Decking
+                        <em class="text-muted">Natur</em>
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4">
+
+            <div class="col">
+                <div class="border p-5">
+                    <a href="#">
+                        <img class="img-fluid" src="{{ asset('images/cm-deking-solid-venge-double.jpg') }}"
+                             alt="Террасная доска из ДПК CM Decking Solid">
+                    </a>
+                    <h3 class="display-6 text-center text-decoration-none">
+                        CM Decking
+                        <em class="text-muted">Solid</em>
+                    </h3>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="border p-5">
+                    <a href="#">
+                        <img class="img-fluid" src="{{ asset('images/cm-deking-solid-venge-double.jpg') }}"
+                             alt="Террасная доска из ДПК CM Decking Solid">
+                    </a>
+                    <h3 class="display-6 text-center text-decoration-none">CM Decking серия Mix</h3>
+                </div>
+            </div>
+
         </div>
 
-        <div class="col p-5">
-            <a href="#">
-                <img class="img-fluid" src="{{ asset('images/Zebrano_Brown_.jpg') }}" alt="">
-            </a>
-            <h3 class="display-6 text-center text-decoration-none">CM Decking Zebrano</h3>
-        </div>
 
-        <div class="col p-5">
-            <a href="#">
-                <img class="img-fluid" src="{{ asset('images/cm-deking-natur-dub-double.jpg') }}" alt="">
-            </a>
-            <h3 class="display-6 text-center text-decoration-none">CM Decking Natur</h3>
-        </div>
+    </div>
 
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
+
+
+
+
+
+
+    <div class="col">
+        <div class="border p-5">
+            <a href="#">
+                <img class="img-fluid" src="{{ asset('images/cm-deking-solid-venge-double.jpg') }}"
+                     alt="Террасная доска из ДПК CM Decking Solid">
+            </a>
+            <h3 class="display-6 text-center text-decoration-none">CM Decking Solid</h3>
+        </div>
+    </div>
     </div>
 
 @endsection
